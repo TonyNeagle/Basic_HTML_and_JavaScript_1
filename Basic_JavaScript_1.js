58fd0eefbf3e51bb2378d93113338aebaa62e435
Tony <tony.neagle@btopenworld.com>
2024-12-08 17:00:53 +0000

    Function  in seperate file
    Placed a function in a .js file to be called from a .html file using src attribute

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+function My_First_Function() {
+		var string = "Kiss me I'm Irish!";
+		var result = string.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
+
+alert("Hello World. I am an alert box");
